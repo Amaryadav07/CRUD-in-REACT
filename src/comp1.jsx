09da27780b8fe558943ee16efa0f451063a1e3cf
Amaryadav07 <anath2893007@gmail.com>
2025-04-16 17:42:24 +0530

    added code

---
+++ src/comp1.jsx
@@ -0,0 +1,12 @@
+
+import Comp2 from "./comp2";
+
+const Comp1=()=>{
+    return(
+        <>
+        <h1>this is comp1</h1>
+        <Comp2  />
+        </>
+    )
+}
+export default Comp1;
---
+++ src/comp3.jsx
@@ -0,0 +1,12 @@
+
+import Comp4 from "./comp4";
+
+const Comp3=()=>{
+    return(
+        <>
+        <h1>this is comp3</h1>
+        <Comp4  />
+        </>
+    )
+}
+export default Comp3;
